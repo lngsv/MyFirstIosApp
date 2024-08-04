ea920b465047b86c3fe99a4deba1161923a85365
Alan Gasiev <gasiev99@gmail.com>
2024-08-03 20:46:01 -0400

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Alan Gasiev on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
